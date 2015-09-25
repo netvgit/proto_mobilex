6dbebdf6576cb21e74d4bd8720771f1154ffea8a
Vivek Agrawal <vivek.agrawal@imaginea.com>
2015-09-25 14:41:13 +0530

    Applied the new theme to the homepage and reports page covering the various functionality involved as per new theme

--- resources/js/reports.js
+++ resources/js/reports.js
@@ -2,10 +2,10 @@ $(function() {
 	if ($.fn.reflect) {
 		//$('#preview-coverflow .cover').reflect();	// only possible in very specific situations
 	}
-	$('#preview-coverflow').coverflow({
-		index:			6,
+	var coverflowObj = $('#preview-coverflow').coverflow({
+		index:			5,
 		density:		2,
-		innerOffset:	50,
+		innerOffset:	30,
 		innerScale:		.7,
 		animateStep:	function(event, cover, offset, isVisible, isMiddle, sin, cos) {
 			if (isVisible) {
@@ -28,36 +28,47 @@ $(function() {
 		change: function(event, cover, index){
 			console.log('change');
 			console.log(arguments);
+			console.log($(cover));
 		},
 		confirm: function(){
 			console.log(arguments);
 			console.log('confirm');
 		},
 		select: function(event, image, index){
-			console.log(arguments);
-			console.log('select');
-			console.log($(image));
-			console.log($(image).attr('data-param1-val'));
-			var imgObj = $(image);
+			var imgObj = $(image),
+				previewImg = $('img.img-preview-ref'),
+				sizeRadioVal = $('input[type=radio][name="switch-size"]:checked:first').val();
 			$('span.param1-val').html(imgObj.attr('data-param1-val'));
 			$('span.param2-val').html(imgObj.attr('data-param2-val'));
 			$('span.param3-val').html(imgObj.attr('data-param3-val'));
-			$('span.param4-val').html(imgObj.attr('data-param4-val'));
+			$('.screen-name-ref').html(imgObj.attr('data-img-name'));
+			previewImg.attr('src', imgObj.attr('src'));
+			//modifySectionsSizeImageEnlarge();
 		}
 	});
-	$('#first-button').click(function() {
-		console.log('first');
-		$('#preview-coverflow').coverflow('index', 0);
+	console.log(coverflowObj);
+	$('input[type=radio][name="switch-size"]').off('change').on('change', function() {
+		modifySectionsSizeImageEnlarge();
 	});
-	$('#last-button').click(function() {
-		console.log('last');
-		$('#preview-coverflow').coverflow('index', -1);
-	});
-	$('#go-button').click(function() {
-		console.log('go');
-		if(!isNaN($('#go-button-value').val()-1)){							
-			$('#preview-coverflow').coverflow('index', $('#go-button-value').val()-1);
-		}
-	});
-
+	function modifySectionsSizeImageEnlarge(){
+		
+		var sizeRadioVal = $('input[type=radio][name="switch-size"]:checked:first').val(),
+			leftSection = $('.left-section-ref'),
+			rightSection = $('.right-section-ref'),
+			previewImg = $('img.img-preview-ref'),
+			coverflowContainerObj = $('#preview'),
+			coverflowObj = $('#preview-coverflow').data('vanderleeCoverflow');
+		if(sizeRadioVal==1){
+			leftSection.removeClass('col-md-6 col-lg-6').addClass('col-md-7 col-lg-7');
+			rightSection.removeClass('col-md-6 col-lg-6').addClass('col-md-5 col-lg-5');
+			previewImg.attr('height', '1136px');
+			previewImg.attr('width', '640px');
+		}else{
+			leftSection.removeClass('col-md-7 col-lg-7').addClass('col-md-6 col-lg-6');
+			rightSection.removeClass('col-md-5 col-lg-5').addClass('col-md-6 col-lg-6');
+			previewImg.attr('height', '568px');
+			previewImg.attr('width', '320px');
+		}		
+		coverflowObj.refresh();
+	}
 });
